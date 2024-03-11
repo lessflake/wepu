d1aa53712f6251de4566f61d8708a74d2b0afea3
ky <138204722+lessflake@users.noreply.github.com>
2024-03-11 05:06:21 +0000

    save/load small books with local storage

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 use std::{cell::RefCell, rc::Rc};
 
+use base64::prelude::*;
 use leptos::*;
 use leptos_router::*;
 use lepu::{Chapter, Content, Epub, Style, Text, TextKind};
 use wasm_bindgen::{closure::Closure, JsCast as _, JsValue};
+
+// Settings
+// - theme
+// - clear storage
+// - use storage
+//   - book (and adjust size threshold?)
+//   - position
 
 fn main() {
     console_error_panic_hook::set_once();
     mount_to_body(App);
 }
 
-type BookResource = Resource<Option<web_sys::File>, Option<Rc<RefCell<Epub>>>>;
+type Book = Option<Rc<RefCell<Epub>>>;
 
-#[derive(Default, Clone, Copy)]
+#[derive(Default, Clone)]
 struct Position {
     page: usize,
     para: Option<usize>,
@@ -22,6 +30,28 @@ struct Position {
 fn App() -> impl IntoView {
     let (source, set_source) = create_signal(None);
     let (pos, set_pos) = create_signal(Position::default());
+
+    let load_saved_pos = move |epub: &Epub| {
+        if let Ok(Some(storage)) = leptos::window().local_storage() {
+            let id = epub.identifier();
+            if let Ok(Some(saved_pos)) = storage.get_item(id) {
+                if let Some((page, para)) = saved_pos.split_once(':') {
+                    let page = page.parse::<usize>().unwrap();
+                    let para = para.parse::<usize>().unwrap();
+                    set_pos.set(Position {
+                        page,
+                        para: Some(para),
+                    });
+                    create_effect(move |_| {
+                        (use_navigate())(&page.to_string(), Default::default());
+                    });
+                }
+            }
+        }
+    };
+
+    let (book, set_book) = create_signal::<Option<Rc<RefCell<Epub>>>>(None);
+
     let res = create_local_resource(
         move || source.get(),
         move |file: Option<web_sys::File>| async move {
@@ -30,33 +60,45 @@ fn App() -> impl IntoView {
             let promise = file.array_buffer();
             let future = wasm_bindgen_futures::JsFuture::from(promise);
             let res = future.await.ok()?;
-            let buf = Uint8Array::new(&res).to_vec();
-            let epub = Epub::new(buf).ok()?;
-
-            if let Ok(Some(storage)) = leptos::window().local_storage() {
-                let id = epub.identifier();
-                if let Ok(Some(saved_pos)) = storage.get_item(id) {
-                    if let Some((page, para)) = saved_pos.split_once(':') {
-                        let page = page.parse::<usize>().unwrap();
-                        let para = para.parse::<usize>().unwrap();
-                        set_pos.set(Position {
-                            page,
-                            para: Some(para),
-                        });
-                        (use_navigate())(&page.to_string(), Default::default());
-                    }
-                }
-            }
-            Some(Rc::new(RefCell::new(epub)))
+            Some(Uint8Array::new(&res).to_vec())
         },
     );
 
+    create_effect(move |_| {
+        let Some(Some(buf)) = res.get() else { return };
+        let encoded = BASE64_STANDARD.encode(&buf);
+        let Ok(epub) = Epub::new(buf) else { return };
+        if let Ok(Some(storage)) = leptos::window().local_storage() {
+            if encoded.len() < 3_000_000 {
+                let _ = storage.set_item("b", &encoded);
+            } else {
+                let _ = storage.remove_item("b");
+            }
+        }
+        load_saved_pos(&epub);
+        // FIXME: not meant to write to signals within effects
+        // reset the resource to save some memory
+        set_source.set(None);
+        set_book.set(Some(Rc::new(RefCell::new(epub))));
+    });
+
+    if let Ok(Some(storage)) = leptos::window().local_storage() {
+        if let Ok(Some(saved_book)) = storage.get_item("b") {
+            if let Ok(data) = BASE64_STANDARD.decode(saved_book) {
+                let epub = Epub::new(data).ok().unwrap();
+                load_saved_pos(&epub);
+                set_book.set(Some(Rc::new(RefCell::new(epub))));
+            }
+        }
+    }
+
     provide_context(pos);
     provide_context(set_pos);
-    provide_context(res);
+    provide_context(book);
+    provide_context(set_book);
 
     let main_view = move || {
-        let book_exists = move || matches!(res.get(), Some(Some(_)));
+        let book_exists = move || matches!(book.get(), Some(_));
         let upload_view = move || {
             view! { <Upload file=set_source /> }
         };
@@ -69,16 +111,17 @@ fn App() -> impl IntoView {
 
     view! {
         <Router base="/wepu">
-        <main>
-            <div class="flex flex-col max-w-screen-sm xl:max-w-screen-md min-h-screen mx-auto space-y-10 py-10 text-2xl">
-                <Routes base="/wepu".to_string()>
-                    <Route path="/" view=main_view>
-                        <Route path="" view=Navigation />
-                        <Route path=":idx" view=Content />
-                    </Route>
-                </Routes>
-            </div>
-        </main>
+            <main>
+                <div class="flex flex-col max-w-screen-sm xl:max-w-screen-md
+                            min-h-screen mx-auto py-10 text-2xl">
+                    <Routes base="/wepu".to_string()>
+                        <Route path="/" view=main_view>
+                            <Route path="" view=Navigation />
+                            <Route path=":idx" view=Content />
+                        </Route>
+                    </Routes>
+                </div>
+            </main>
         </Router>
     }
 }
@@ -103,7 +146,15 @@ fn Upload(file: WriteSignal<Option<web_sys::File>>) -> impl IntoView {
                        bg-1 border-dashed border-zinc-600 border-2
                        aspect-square w-4/12 active:bg-zinc-800">
             </button>
-            <input on:input=on_submit class="hidden" tabindex=-1 type="file" id="upload" name="book" accept="application/epub+zip" node_ref=input_element />
+            <input
+                on:input=on_submit
+                class="hidden"
+                tabindex=-1
+                type="file"
+                id="upload"
+                name="book"
+                accept="application/epub+zip"
+                node_ref=input_element />
         </div>
     }
 }
@@ -118,30 +169,50 @@ fn Navigation() -> impl IntoView {
                 let idx = e.index_in_spine().to_string();
                 let name = e.name().to_owned();
                 let class = if e.index_in_spine() == pos.get().page {
-                    "text-sky-400"
+                    "text-sky-100"
                 } else {
                     "text-sky-300"
                 };
                 view! {
-                    <li><A href=idx class=class>{name}</A>
+                    <li><div class="pb-2"><A href=idx class=class>{name}</A></div>
                         {sublist}
                     </li>
                 }
             })
             .collect_view();
 
-        html::ul()
-            .classes("ml-5 space-y-2 pt-2 pb-4")
-            .child(inner)
-            .into_view()
+        html::ul().classes("ml-5").child(inner).into_view()
     }
 
+    let unload_book = move || {
+        let set_book = expect_context::<WriteSignal<Book>>();
+        set_book.set(None);
+        if let Ok(Some(storage)) = leptos::window().local_storage() {
+            let _ = storage.remove_item("b");
+        }
+    };
+
+    let handle = window_event_listener(ev::keyup, move |ev: ev::KeyboardEvent| {
+        if ev.alt_key() || ev.shift_key() || ev.meta_key() || ev.ctrl_key() {
+            return;
+        }
+        match &*ev.key() {
+            "Escape" => unload_book(),
+            _ => {}
+        }
+    });
+
+    on_cleanup(move || handle.remove());
+
     view! {
+        <div class="absolute top-2 left-5 text-4xl font-thin">
+            <button on:click=move |_| unload_book()>"✕"</button>
+        </div>
         { move || {
-            let book = expect_context::<BookResource>().get().unwrap().unwrap();
+            let book = expect_context::<ReadSignal<Book>>().get().unwrap();
             let b = book.borrow();
             view! {
-                <div class="pt-8 pb-6 text-left font-sans font-bold text-4xl tracking-tight leading-none">
+                <div class="mt-8 mb-10 text-left font-sans font-bold text-4xl tracking-tight leading-none">
                     <h1>{b.title().to_owned()}</h1>
                 </div>
                 <div class="text-justify font-serif">
@@ -175,12 +246,12 @@ fn convert(text: &Text<'_>) -> leptos::View {
     }
 
     match text.kind() {
-        TextKind::Header => html::div()
+        TextKind::Header => html::h1()
             .attr(
                 "class",
-                "mt-8 mb-6 text-left font-sans font-bold text-4xl tracking-tight leading-none",
+                "mt-8 mb-10 text-left font-sans font-bold text-4xl tracking-tight leading-none",
             )
-            .child(html::h1().child(children))
+            .child(children)
             .into_view(),
         TextKind::Paragraph => html::p().child(children).into_view(),
         TextKind::Quote => html::blockquote()
@@ -200,18 +271,20 @@ fn Content() -> impl IntoView {
     let params = use_params::<ChapterParams>();
     let pos = expect_context::<ReadSignal<Position>>();
     let set_pos = expect_context::<WriteSignal<Position>>();
-    let book = expect_context::<BookResource>();
+    let book = expect_context::<ReadSignal<Book>>();
     let page = move || match params.with(|p| p.as_ref().map(|p| p.idx).ok().flatten()) {
         Some(page) => page,
         None => {
-            // HACK: trailing slash makes this route match
-            (use_navigate())(
-                "/",
-                NavigateOptions {
-                    replace: true,
-                    ..Default::default()
-                },
-            );
+            if book.get().is_none() {
+                // HACK: trailing slash makes this route match
+                (use_navigate())(
+                    "/",
+                    NavigateOptions {
+                        replace: true,
+                        ..Default::default()
+                    },
+                );
+            }
             0
         }
     };
@@ -229,11 +302,12 @@ fn Content() -> impl IntoView {
         let Ok(Some(storage)) = leptos::window().local_storage() else {
             return;
         };
-        let Some(Some(book)) = book.get() else { return };
+        let Some(book) = book.get() else { return };
         let book = book.borrow();
         let id = book.identifier();
         let page = page();
         if para != 0 {
+            // FIXME: not meant to write to signals within effects
             set_pos.update(move |pos| pos.para = Some(para));
         }
         if let Err(e) = storage.set_item(id, &format!("{page}:{para}")) {
@@ -254,22 +328,16 @@ fn Content() -> impl IntoView {
 
     let cb: Closure<dyn Fn(Vec<JsValue>, web_sys::IntersectionObserver)> = Closure::new(cb);
     let obs = web_sys::IntersectionObserver::new(cb.as_ref().unchecked_ref()).unwrap();
-
-    {
-        let clean_obs = obs.clone();
-        on_cleanup(move || {
-            clean_obs.disconnect();
-            drop(cb)
-        });
-    }
+    let cleanup_obs = obs.clone();
 
     let text = move || {
         let mut out: Vec<View> = Vec::new();
 
         let mut id = 0;
-        let book = book.get().unwrap().unwrap();
+        let book = book.get().unwrap();
+        let page = page();
         book.borrow_mut()
-            .traverse_chapter(page(), |content, _| {
+            .traverse_chapter(page, |content, _| {
                 let view = match content {
                     Content::Textual(tc) => convert(&tc).into_view(),
                     Content::Image => view! {}.into_view(),
@@ -280,7 +348,8 @@ fn Content() -> impl IntoView {
                     .child(view)
                     .on_mount(move |node| {
                         obs.observe(&node);
-                        if Some(id) == pos.get_untracked().para && id != 0 {
+                        let pos = pos.get_untracked();
+                        if Some(id) == pos.para && id != 0 && page == pos.page {
                             create_effect(move |_| {
                                 node.scroll_into_view();
                             });
@@ -292,6 +361,12 @@ fn Content() -> impl IntoView {
             .unwrap();
 
         Some(out.into_iter().collect_view())
+    };
+
+    let leave = move || {
+        set_pos.update(|pos| pos.para = None);
+        set_vs.set(Default::default());
+        (use_navigate())("/", Default::default());
     };
 
     let navigate = use_navigate();
@@ -317,19 +392,27 @@ fn Content() -> impl IntoView {
             }
             "ArrowRight" => {
                 let id = page();
-                let max_id = book.get().unwrap().unwrap().borrow().document_count();
+                let max_id = book.get().unwrap().borrow().document_count();
                 if id + 1 >= max_id {
                     return;
                 };
                 move_page(id + 1);
             }
-            "Escape" => navigate("/", Default::default()),
+            "Escape" => leave(),
             _ => {}
         }
     });
-    on_cleanup(move || handle.remove());
+
+    on_cleanup(move || {
+        cleanup_obs.disconnect();
+        handle.remove();
+        drop(cb);
+    });
 
     view! {
+        <div class="absolute top-2 left-5 text-4xl font-thin">
+            <button on:click=move |_| leave()>"✕"</button>
+        </div>
         {move || {
             view! {
                 <div class="text-justify font-serif font-light space-y-5
