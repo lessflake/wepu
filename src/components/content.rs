7ef442ace774bc1d8e09b2fb672f5b1d04eb8b33
ky <138204722+lessflake@users.noreply.github.com>
2024-03-18 23:47:06 +0000

    move position tracking into position module

--- src/components/content.rs
+++ src/components/content.rs
@@ -1,27 +1,16 @@
-use std::{
-    cell::RefCell,
-    collections::{BTreeMap, BTreeSet},
-};
+use std::{cell::RefCell, collections::BTreeMap, rc::Rc};
 
 use base64::prelude::*;
 use leptos::*;
 use leptos_router::*;
 use lepu::{Content, Style, Text, TextKind};
-use wasm_bindgen::{closure::Closure, JsCast as _, JsValue};
 
-use crate::{book::Book, config, input, nav_state::NavState, set_nav_state, Marks};
+use crate::{book::Book, input, nav_state::NavState, position, set_nav_state, Marks};
 
 #[derive(Params, Debug, Clone, PartialEq)]
 struct ChapterParams {
     idx: Option<usize>,
 }
-
-// what is going on here?
-// - sets nav state
-// - keyboard handler
-//   - changes page
-//   - changes input handler state
-//   - changes marks
 
 #[component]
 pub fn Content() -> impl IntoView {
@@ -32,9 +21,8 @@ pub fn Content() -> impl IntoView {
 
     set_nav_state(NavState::Read);
 
-    let pos = expect_context::<ReadSignal<BTreeMap<usize, usize>>>();
     let set_pos = expect_context::<WriteSignal<BTreeMap<usize, usize>>>();
-    let cur_page = expect_context::<ReadSignal<usize>>();
+    let page = expect_context::<ReadSignal<usize>>();
     let set_page = expect_context::<WriteSignal<usize>>();
     let book = expect_context::<ReadSignal<Book>>();
     let param_page = move || {
@@ -44,124 +32,30 @@ pub fn Content() -> impl IntoView {
     };
     create_effect(move |_| set_page.set(param_page()));
 
-    let (vs, set_vs) = create_signal(BTreeSet::<usize>::new());
-    let first_visible_block = create_memo(move |_| vs.get().iter().min().copied());
+    let tracker = Rc::new(position::init_tracking());
 
-    let config = config::get();
-    create_effect(move |_| {
-        let Some(para) = first_visible_block.get() else {
-            return;
-        };
-        let Ok(Some(storage)) = leptos::window().local_storage() else {
-            return;
-        };
-        let Some(book) = book.get() else { return };
-        let id = book.identifier();
-        let page = param_page();
-        set_pos.update(move |pos| {
-            if para > 1 {
-                pos.insert(page, para);
-            } else {
-                pos.remove(&page);
-            }
-        });
-        if config.borrow().save_position {
-            let _ = storage.set_item(id, &format!("{page}:{para}"));
-        }
-    });
-
-    let cb = move |entries, _| {
-        for entry in entries {
-            let entry = web_sys::IntersectionObserverEntry::from(entry);
-            let id = entry.target().id().parse::<usize>().unwrap();
-            match entry.is_intersecting() {
-                true => set_vs.update(|vs| _ = vs.insert(id)),
-                false => set_vs.update(|vs| _ = vs.remove(&id)),
-            }
-        }
-    };
-
-    let cb: Closure<dyn Fn(Vec<JsValue>, web_sys::IntersectionObserver)> = Closure::new(cb);
-    let obs = web_sys::IntersectionObserver::new(cb.as_ref().unchecked_ref()).unwrap();
-    let cleanup_obs = obs.clone();
-
-    let text = move || {
-        let mut out: Vec<View> = Vec::new();
-
-        let mut id = 0;
-        let book = book.get().unwrap();
-        let page = param_page();
-        let cur_page = cur_page.get();
-        book.traverse_chapter(page, |ctx, content, _| {
-            let view = match content {
-                Content::Textual(tc) => convert(&tc).into_view(),
-                Content::Image(item) => {
-                    let Ok(data) = ctx.load(&item) else { return };
-                    let mime = item.mime();
-                    let mut data_string = format!("data:{mime};base64,");
-                    BASE64_STANDARD.encode_string(&data, &mut data_string);
-                    view! {
-                        <div class="flex justify-center py-2">
-                            <img src=data_string />
-                        </div>
-                    }
-                    .into_view()
-                }
-            };
-            let obs = obs.clone();
-            let view = html::div()
-                .id(id.to_string())
-                .child(view)
-                .on_mount(move |node| {
-                    obs.observe(&node);
-                    pos.with_untracked(move |pos| {
-                        if Some(id) == pos.get(&page).copied() && id != 0 && page == cur_page {
-                            create_effect(move |_| {
-                                node.scroll_into_view();
-                            });
-                        }
-                    });
-                });
-            out.push(view.into_view());
-            id += 1;
-        })
-        .unwrap();
-
-        Some(out.into_iter().collect_view())
-    };
-
-    let leave = move || {
-        set_vs.set(Default::default());
-        (use_navigate())("/", Default::default());
-    };
-
+    let tracker_ = tracker.clone();
     let navigate = use_navigate();
     let handler = RefCell::new(input::Handler::new());
     let marks = expect_context::<Marks>();
 
-    let move_page = move |id| {
-        set_page.set(id);
-        set_vs.set(Default::default());
-        navigate(&id.to_string(), Default::default());
-    };
-
-    let move_page_ = move_page.clone();
     let move_next = move || {
         let id = param_page();
         let max_id = book.get().unwrap().document_count();
         if id + 1 < max_id {
-            move_page_(id + 1);
+            navigate(&(id + 1).to_string(), Default::default());
         }
     };
 
-    let move_page_ = move_page.clone();
+    let navigate = use_navigate();
     let move_previous = move || {
         let id = param_page();
         if id > 0 {
-            move_page_(id - 1);
+            navigate(&(id - 1).to_string(), Default::default());
         }
     };
 
+    let navigate = use_navigate();
     let move_next_ = move_next.clone();
     let move_previous_ = move_previous.clone();
     let handle = window_event_listener(ev::keyup, move |ev: ev::KeyboardEvent| {
@@ -177,7 +71,7 @@ pub fn Content() -> impl IntoView {
             Action::NextPage => move_next_(),
             Action::PreviousPage => move_previous_(),
             Action::SetMark(c) => {
-                let Some(para) = first_visible_block.get() else {
+                let Some(para) = tracker_.first_visible() else {
                     return;
                 };
                 let page = param_page();
@@ -186,24 +80,23 @@ pub fn Content() -> impl IntoView {
             Action::FollowMark(c) => {
                 if let Some((page, para)) = marks.borrow().get(&c).copied() {
                     set_pos.update(move |pos| _ = pos.insert(page, para));
-                    move_page(page);
+                    navigate(&page.to_string(), Default::default());
                 }
             }
-            Action::Leave => leave(),
+            Action::Leave => navigate("/", Default::default()),
         }
     });
 
     on_cleanup(move || {
-        cleanup_obs.disconnect();
         handle.remove();
-        drop(cb);
     });
 
     view! {
         {move || {
+            let tracker = tracker.clone();
             view! {
                 <div class="sm:text-justify font-serif font-light space-y-3 md:space-y-5 mt-8">
-                    {text()}
+                    {text(tracker, book, page)}
                 </div>
             }
         }}
@@ -220,6 +113,45 @@ pub fn Content() -> impl IntoView {
             </div>
         </div>
     }
+}
+
+fn text(
+    tracker: Rc<position::Tracker>,
+    book: ReadSignal<Book>,
+    page: ReadSignal<usize>,
+) -> impl IntoView {
+    let mut out: Vec<View> = Vec::new();
+
+    let mut id = 0;
+    let book = book.get().unwrap();
+    let page = page.get();
+    book.traverse_chapter(page, |ctx, content, _| {
+        let view = match content {
+            Content::Textual(tc) => convert(&tc).into_view(),
+            Content::Image(item) => {
+                let Ok(data) = ctx.load(&item) else { return };
+                let mime = item.mime();
+                let mut data_string = format!("data:{mime};base64,");
+                BASE64_STANDARD.encode_string(&data, &mut data_string);
+                view! {
+                    <div class="flex justify-center py-2">
+                        <img src=data_string />
+                    </div>
+                }
+                .into_view()
+            }
+        };
+        let tracker = tracker.clone();
+        let view = html::div()
+            .id(id.to_string())
+            .child(view)
+            .on_mount(move |node| tracker.track(node, id, page));
+        out.push(view.into_view());
+        id += 1;
+    })
+    .unwrap();
+
+    Some(out.into_iter().collect_view())
 }
 
 fn convert(text: &Text<'_>) -> leptos::View {
--- src/position.rs
+++ src/position.rs
@@ -1,6 +1,7 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 
 use leptos::*;
+use wasm_bindgen::{closure::Closure, JsCast as _, JsValue};
 
 use crate::{book::Book, config};
 
@@ -37,4 +38,94 @@ pub fn init() {
         set_page.set(page);
         set_pos.update(|pos| _ = pos.insert(page, para));
     });
+}
+
+pub struct Tracker {
+    obs: web_sys::IntersectionObserver,
+    first_visible: Memo<Option<usize>>,
+    pos: ReadSignal<BTreeMap<usize, usize>>,
+}
+
+impl Tracker {
+    pub fn track(&self, node: HtmlElement<html::Div>, id: usize, page: usize) {
+        self.obs.observe(&node);
+        self.pos.with_untracked(move |pos| {
+            if Some(id) == pos.get(&page).copied() && id != 0 {
+                create_effect(move |_| {
+                    node.scroll_into_view();
+                });
+            }
+        });
+    }
+
+    pub fn first_visible(&self) -> Option<usize> {
+        self.first_visible.get()
+    }
+}
+
+pub fn init_tracking() -> Tracker {
+    let (vs, set_vs) = create_signal(BTreeSet::<usize>::new());
+    let first_visible = create_memo(move |_| vs.get().iter().min().copied());
+
+    let book = expect_context::<ReadSignal<Book>>();
+    let page = expect_context::<ReadSignal<usize>>();
+    let pos = expect_context::<ReadSignal<BTreeMap<usize, usize>>>();
+    let set_pos = expect_context::<WriteSignal<BTreeMap<usize, usize>>>();
+
+    create_effect(move |prev| {
+        let cur = page.get();
+        if prev != Some(cur) {
+            set_vs.set(Default::default());
+        }
+        cur
+    });
+
+    let config = config::get();
+    create_effect(move |_| {
+        let Some(para) = first_visible.get() else {
+            return;
+        };
+        let Ok(Some(storage)) = leptos::window().local_storage() else {
+            return;
+        };
+        let Some(book) = book.get() else { return };
+        let page = page.get();
+        let id = book.identifier();
+        set_pos.update(move |pos| {
+            if para > 1 {
+                pos.insert(page, para);
+            } else {
+                pos.remove(&page);
+            }
+        });
+        if config.borrow().save_position {
+            let _ = storage.set_item(id, &format!("{page}:{para}"));
+        }
+    });
+
+    let cb = move |entries, _| {
+        for entry in entries {
+            let entry = web_sys::IntersectionObserverEntry::from(entry);
+            let id = entry.target().id().parse::<usize>().unwrap();
+            match entry.is_intersecting() {
+                true => set_vs.update(|vs| _ = vs.insert(id)),
+                false => set_vs.update(|vs| _ = vs.remove(&id)),
+            }
+        }
+    };
+
+    let cb: Closure<dyn Fn(Vec<JsValue>, web_sys::IntersectionObserver)> = Closure::new(cb);
+    let obs = web_sys::IntersectionObserver::new(cb.as_ref().unchecked_ref()).unwrap();
+    let cleanup_obs = obs.clone();
+
+    on_cleanup(move || {
+        cleanup_obs.disconnect();
+        drop(cb);
+    });
+
+    Tracker {
+        obs,
+        first_visible,
+        pos,
+    }
 }
